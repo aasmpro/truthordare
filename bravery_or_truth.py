0244aaf9ab88875554b7916fc83c1a611e951136
Abolfazl Amiri <aa.smpro@gmail.com>
2017-11-26 02:53:44 +0330

    adding last_player_2
    for preventing same persons ahead.

--- bravery_or_truth.py
+++ bravery_or_truth.py
@@ -9,11 +9,13 @@ from random import *
 players_num = int(input('number of players : '))
 players = [[input(('player {} name : ').format(i + 1)) or str(i + 1), 'truth'] for i in range(0, players_num)]
 last_player = randint(0, players_num - 1)
+last_player_2 = last_player
 exchange = {"truth": "bravery", "bravery": "truth", }
 while True:
     next_player = randint(0, players_num - 2)
-    if last_player is not next_player:
+    if last_player is not next_player and next_player is not last_player_2:
         print('{}  :  {}  >>  {}'.format(players[last_player][0], players[next_player][0], players[next_player][1]))
         players[next_player][1] = exchange[players[next_player][1]]
+        last_player_2 = last_player
         last_player = next_player
         if input() == 'q': break
